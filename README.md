6b4cf7847ef52bcdd40469f9f8af1537cc75066e
Lincoln Auster <lincolnauster@gmail.com>
2020-08-08 17:36:24 -0600

    ini file is now somewhat useful

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Dzen Blocks
+# dzen-blocks
 Easily spawn multiple dzen instances for different blocks.
 
 ![screenshot](screenshot.png)
@@ -9,3 +9,17 @@ dzen-blocks reads data in from a config file to create multiple bars, i.e.:
 `ptyhon3 dzen-blocks.py dzen-blocks-config.ini`
 
 A sample config file is provided in this repo.
+
+## Configuration
+A config file is in ini format, and specifies the following for every block:
+* The command to run. This includes simple commands like date as well as complex shell scripts.
+2) 
+* Any additional options to pass to dzen (extra flags, etc)
+and the following global values:
+* x, y offset: where the first block is aligned
+* x padding: distance between blocks
+
+## Tentatively Planned Additions/Roadmap
+* more global flags, including font and colors
+* not needing to escape %'s in the config file
+* adding this into some package managers
--- dzen-blocks.py
+++ dzen-blocks.py
@@ -6,6 +6,9 @@ class dzen:
     def __init__(self):
         self.cmd = "echo 'No command found in config'"
         self.options = "-p"
+        self.xoffset = 0
+        self.yoffset = 0
+        self.width = 0
     def run(self):
         write = "while sleep 1; do (" + self.cmd + "); done | dzen2 " + self.options
         print(write)
@@ -15,9 +18,20 @@ class dzen:
         self.cmd = command
     def SetOptions(self, options):
         self.options = options
+    def SetWidth(self, width):
+        self.width = width
+        self.options += " -w " + str(width)
+    def SetXOFFSET(self, xoff):
+        self.xoffset = xoff
+        self.options += " -x " + str(self.xoffset)
+    def SetYOFFSET(self, yoff):
+        self.yoffset = yoff
+        self.options += " -y " + str(self.yoffset)
+
+    def GetWidth(self):
+        return self.width
 
 if __name__ == "__main__":
-    # read ini file from -c or XDG_CONFIG_HOME
     parser = argparse.ArgumentParser(description="Manage dzen instances")
     parser.add_argument("config", help="Config file")
 
@@ -27,15 +41,34 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read(config_path)
 
-    for section in config.sections():
-        # run dzen with options in config
-        dzen_instance = dzen()
-        for key in config[section]:
-            if key == "cmd":
-                dzen_instance.SetCommand(config[section][key])
-            elif key == "options":
-                dzen_instance.SetOptions(config[section][key])
+    CONSTS= "global"
+    xoffset  = int(config[CONSTS]["xoffset" ]);
+    YOFFSET  = int(config[CONSTS]["yoffset" ]);
+    XPADDING = int(config[CONSTS]["xpadding"]);
 
-        dzen_instance.run()
 
-        print()
+    for count in range(len(config.sections())):
+        section = config.sections()[count]
+        if section != CONSTS:
+            dzeninstance = dzen()
+            if "cmd" not in config[section]:
+                dzeninstance.SetCommand("echo 'cmd not found'")
+            else:
+                dzeninstance.SetCommand(config[section]["cmd"])
+
+
+            if "opt" in config[section]:
+                dzeninstance.SetOptions(config[section]["opt"])
+            if "width" in config[section].keys():
+                dzeninstance.SetWidth(int(config[section]["width"]))
+            else:
+                dzeninstance.SetWidth(128)
+
+            dzeninstance.SetXOFFSET(xoffset)
+            dzeninstance.SetYOFFSET(YOFFSET)
+
+            dzeninstance.run()
+
+
+            xoffset += XPADDING
+            xoffset += dzeninstance.GetWidth()
