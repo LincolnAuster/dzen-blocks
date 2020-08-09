fd4a1315d42cd91e7e53f67c7a98477a82fbca5f
Lincoln Auster <lincolnauster@gmail.com>
2020-08-09 16:48:11 -0600

    cleaned everything up a bit, it's now easier to kill processes

--- dzen-blocks.py
+++ dzen-blocks.py
@@ -3,6 +3,7 @@ import sys, os
 
 import subprocess
 import signal
+
 import configparser
 
 class dzen:
@@ -29,41 +30,49 @@ class dzen:
         self.options += options
     def SetWidth(self, width):
         self.width = width
+        # at some point it would probably be best
+        # to write some code to remove the w flag
+        # before adding it back on.
         self.options += " -w " + str(width)
     def SetXOFFSET(self, xoff):
+        # same thing here with x: remove the flag
         self.xoffset = xoff
         self.options += " -x " + str(self.xoffset)
     def SetYOFFSET(self, yoff):
+        # same here
         self.yoffset = yoff
         self.options += " -y " + str(self.yoffset)
 
     def GetWidth(self):
         return self.width
-    def Kill(self):
+
+    def Clean(self):
         os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
 
 def h():
     print("Usage: dzen-blocks [config]")
-    sys.exit()
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         h()
-
+        sys.exit()
     if sys.argv[1] == "h" or sys.argv[1] == "help":
         h()
-    else:
-        config_path = sys.argv[1]
+        sys.exit()
 
+    config_path = sys.argv[1]
     config = configparser.ConfigParser()
     config.read(config_path)
 
-    # specify name of global options
     CONSTS = "global"
 
-    XOFFSET  = int(config[CONSTS]["xoffset" ]);
-    YOFFSET  = int(config[CONSTS]["yoffset" ]);
-    XPADDING = int(config[CONSTS]["xpadding"]);
+    try:
+        XOFFSET  = int(config[CONSTS]["xoffset" ]);
+        YOFFSET  = int(config[CONSTS]["yoffset" ]);
+        XPADDING = int(config[CONSTS]["xpadding"]);
+    except KeyError:
+        print("Missing xoffset, yoffset, or xpadding in global config.")
+        sys.exit()
 
     xpos = XOFFSET
 
@@ -103,11 +112,13 @@ if __name__ == "__main__":
             xpos += XPADDING
             xpos += dzeninstance.GetWidth()
 
-    def on_exit(sig, frame):
+    def exit():
         for instance in dzens:
-            instance.Kill()
+            instance.Clean()
+    def on_exit(sig, frame):
+        exit()
 
     signal.signal(signal.SIGINT, on_exit)
     input("Ctrl+C or enter kills dzen instances.")
 
-    on_exit()
+    exit()
